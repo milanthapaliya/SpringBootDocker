0e224f67cec050e08ea2f59570a8b6874f9a1b75
milanthapaliya <thapaliyamilan8@gmail.com>
2024-03-26 14:39:23 +0545

    Springboot with Docker initial setup with Postgresql

---
+++ src/main/java/com/example/sprintbootdockerapplicaiton/SprintBootDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootdockerapplicaiton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootDockerApplicaitonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootDockerApplicaitonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sprintbootdockerapplicaiton/SprintBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootdockerapplicaiton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootDockerApplicaitonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
